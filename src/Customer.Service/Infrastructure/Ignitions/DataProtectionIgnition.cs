3c5e8628714055ab4d4398b2d438777b69675223
Lóan Burger <loan.burger@gmail.com>
2023-04-25 13:01:33 +1200

    Add dataprotection key folder

---
+++ src/Customer.Service/Infrastructure/Ignitions/DataProtectionIgnition.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
+
+namespace Customer.Service.Ignition
+{
+    public static class DataProtectionIgnition
+    {
+        public static void ConfigureDataProtection(this WebApplicationBuilder builder)
+        {
+
+            var keysPath = builder.Configuration["Platform:Data_Protection_Keys_Path"];
+            DirectoryInfo directoryInfo = new (keysPath);
+
+            // Would be beter to save the keys to something like AWS param store
+            builder.Services.AddDataProtection()
+                .PersistKeysToFileSystem(directoryInfo)                
+                 .UseCryptographicAlgorithms(new AuthenticatedEncryptorConfiguration()
+                 {
+                     EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
+                     ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
+                 });
+        }
+    }
+}
